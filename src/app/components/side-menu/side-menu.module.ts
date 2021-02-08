edb948175fa03c2348a444784597cc079ab5eb97
afifashroff <afif.ashroff@aerodyne.co>
2021-02-09 00:01:52 +0800

    side-menu, app-routing: Setup side-menu to handle routing properly

--- src/app/components/side-menu/side-menu.module.ts
+++ src/app/components/side-menu/side-menu.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideMenuComponent } from './side-menu.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SideMenuComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [SideMenuComponent]
 })
