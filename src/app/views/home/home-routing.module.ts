9597e78619515ff68a7a2c88f0148cf885ad86f1
afifashroff <afif.ashroff@aerodyne.co>
2021-02-13 10:45:37 +0800

    styles, app-routing, home.module, about-us, default: Setup routing to follow original site

--- src/app/views/home/home-routing.module.ts
+++ src/app/views/home/home-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './default/default.component';
 import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: DefaultComponent
+      },
+      {
+        path: 'about-us',
+        loadChildren: () => import('../about-us/about-us.module').then(m => m.AboutUsModule)
+      },
+      {
+        path: '**',
+        pathMatch: 'full',
+        redirectTo: 'about-us'
+      }
+    ]
   }
 ];
 
--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { DefaultComponent } from './default/default.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, DefaultComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
